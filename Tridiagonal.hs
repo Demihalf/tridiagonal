dd36f52140d0b4eecb52c2c5c58e755f93474f14
Valery Kharitonov <kharvd@gmail.com>
2014-04-04 22:01:19 +0400

    added random input

--- Tridiagonal.hs
+++ Tridiagonal.hs
@@ -1,6 +1,7 @@
 import System.IO
 import System.Environment
 import System.Exit
+import System.Random
 
 import Data.List
 import Data.Maybe
@@ -28,6 +29,9 @@ readDoubleList = map readD . B.words
 nextVector :: IO [Double]
 nextVector = liftM readDoubleList B.getLine
 
+randomVector :: (Random a) => Int -> (a, a) -> IO [a]
+randomVector n r = replicateM n (randomRIO r)
+
 -- | Извлекает коэффициенты уравнения из stdin
 getEqDataStdIn :: IO (EquationData Double)
 getEqDataStdIn = do [a, b, c, d] <- replicateM 4 nextVector
@@ -35,7 +39,9 @@ getEqDataStdIn = do [a, b, c, d] <- replicateM 4 nextVector
 
 -- | Генерирует случайный набор значений коэффициентов
 getEqDataRandom :: Int -> IO (EquationData Double)
-getEqDataRandom = undefined
+getEqDataRandom n = do [a, c] <- replicateM 2 (randomVector (n-1) (-1000.0, 1000.0))
+                       [b, d] <- replicateM 2 (randomVector n (-1000.0, 1000.0))
+                       return $ EquationData (0:a) b (c ++ [0]) d
 
 -- | Аргумент соответствует optRandom
 getEqData :: Maybe Int -> IO (EquationData Double)
@@ -47,6 +53,7 @@ maybeShowUsage :: Bool -> IO ()
 maybeShowUsage True = putStr usageMessage >> exitSuccess
 maybeShowUsage _    = return ()
 
+-- | Выводит список в stdin и переводит строку
 printListLn :: (Show a) => [a] -> IO ()
 printListLn xs = mapM_ (\x -> putStr (show x ++ " ")) xs >> putStr "\n"
 
@@ -54,7 +61,11 @@ testingMode :: Bool -> Maybe (EquationSolution Double) -> IO ()
 testingMode True (Just sol) = do x <- nextVector
                                  printListLn x
                                  print $ dist x sol
-testingMode _ _             = return ()
+testingMode _    _          = return ()
+
+printEqData :: Bool -> EquationData Double -> IO ()
+printEqData True (EquationData a b c d) = mapM_ printListLn [a, b, c, d]
+printEqData _    _                      = return ()
 
 printSolution :: Maybe (EquationSolution Double) -> IO ()
 printSolution (Just sol) = printListLn sol
@@ -63,18 +74,22 @@ printSolution _          = noSolutionExit
 noSolutionExit :: IO ()
 noSolutionExit = hPutStrLn stderr "Не удалось найти решение" >> exitFailure
 
-solveEquation :: Method -> EquationData Double -> Maybe (EquationSolution Double)
-solveEquation MethodTDMA     = TDMA.getSolution
-solveEquation MethodUnstable = undefined
-
 -- | Считает норму разности между векторами a и b
 dist :: (Real a) => [a] -> [a] -> a
 dist a b = foldl1' max $ map abs $ zipWith (-) a b
 
+solveEquation :: Method -> EquationData Double -> Maybe (EquationSolution Double)
+solveEquation MethodTDMA     = TDMA.getSolution
+solveEquation MethodUnstable = undefined
+
 main :: IO ()
 main = do opts <- getArgs >>= programOpts
           maybeShowUsage (optHelp opts)
+
           eqd <- getEqData (optRandom opts) 
+          printEqData (isJust $ optRandom opts) eqd
+
           let sol = solveEquation (optMethod opts) eqd
           printSolution sol
-          testingMode (optTesting opts) sol
+          testingMode (optTesting opts && (isNothing $ optRandom opts)) sol
+
